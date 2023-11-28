2d0f67b252a127dfd640303a6282892583b00e79
gogoi2k20 <gogoi2k20@gmail.com>
2023-11-29 01:15:44 +0530

    fix: bucket region

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "vprofileactions2k23"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-west-2"
   }
 
   required_version = "~> 1.6.3"
