5cae9e7c813464d4b41c8f156a927782ef241788
gogoi2k20 <gogoi2k20@gmail.com>
2023-11-29 01:51:45 +0530

    test

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,12 +1,8 @@
-locals {
-  cluster_name = "vprofile-eks"
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.19.1"
 
-  cluster_name    = local.cluster_name
+  cluster_name    = "vprofile-eks"
   cluster_version = "1.27"
 
   vpc_id                         = module.vpc.vpc_id
